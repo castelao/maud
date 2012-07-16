610a23603fc222ff4e2e34cf681ace56d1406511
Bia Melo Villas Boas <melovillasboas@gmail.com>
2012-07-16 16:48:04 -0300

    Testing window_1Dmean for triangular window

--- testing_conv.py
+++ testing_conv.py
@@ -1,8 +1,9 @@
 import numpy as np
 from numpy import ma, pi
-from uneven_window_mean import window_1Dmean
+from maud import window_1Dmean
 import pylab
 from window_func import _weight_blackman as blackman
+from window_func import _weight_triangular as triangular
 
 x =  ma.array(np.sort(100*np.random.random(100)))
 y = 5*np.sin(2*pi*x/43.) + 3*np.sin(2*pi*(x-7.)/12.) + 2*np.random.rand(len(x))-1
@@ -23,12 +24,14 @@ for i in np.arange(len(x)):
 
 zf = window_1Dmean(y,l1,method='blackman')
 zf2 = window_1Dmean(y,l1,method='hann')
+zf3 = window_1Dmean(y,l1,method='triangular')
 
 pylab.figure()
 pylab.plot(x,y)
-pylab.plot(x,yf,'r')
-pylab.plot(x,y2f,'og')
-pylab.plot(x,y-y2f,'k')
-pylab.plot(x,zf,'xr')
-pylab.plot(x,zf2,'xb')
+#pylab.plot(x,yf,'r')
+#pylab.plot(x,y2f,'og')
+#pylab.plot(x,y-y2f,'k')
+#pylab.plot(x,zf,'r')
+#pylab.plot(x,zf2,'b')
+pylab.plot(x,zf3,'g')
 pylab.show()
