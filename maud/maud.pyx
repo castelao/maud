2b8cdfda433f464c745857d960fb3d3b961193dc
Capt. Dr. Guilherme Castelao <guilherme@castelao.net>
2013-07-01 15:54:54 +0200

    Working on cython version of window_1Dmean()

--- maud/maud.pyx
+++ maud/maud.pyx
@@ -1,3 +1,5 @@
+from maud.cwindow_func import window_func
+
 import numpy as np
 from numpy import ma
 
@@ -7,3 +9,64 @@ from libc.math cimport cos
 DTYPE = np.float64
 ctypedef np.float64_t DTYPE_t
 
+
+def window_1Dmean(data, double l, t=None, method='hann', axis=0, parallel=True):
+    """ A moving window mean filter, not necessarily a regular grid.
+
+        1D means that the filter is applied to along only one
+          of the dimensions, but in the whole array. For example
+          in a 3D array, each latXlon point is filtered along the
+          time.
+
+        It's not optimized for a regular grid.
+
+        t is the scale of the choosed axis
+
+        l is the size of the filter.
+    """
+    assert axis <= data.ndim, "Invalid axis!"
+
+    if axis != 0:
+        data_smooth = window_1Dmean(data.swapaxes(0,axis),
+            l = l,
+            t = t,
+            method = method,
+            axis=0,
+            parallel = parallel)
+
+        return data_smooth.swapaxes(0,axis)
+
+    if t == None:
+        print "The scale along the choosed axis weren't defined. I'll consider a constant sequence."
+        t = np.arange(data.shape[axis])
+
+    elif t.shape != (data.shape[axis],):
+        print "Invalid size of t."
+        return
+    # ----
+    winfunc = window_func(method)
+
+    data_smooth = ma.masked_all(data.shape)
+
+    cdef int i
+
+    if data.ndim==1:
+        # It's faster than getmaskarray
+        (I,) = np.nonzero(np.isfinite(data))
+        for i in I:
+            dt = t-t[i]
+            ind = np.nonzero((np.absolute(dt)<l))
+            w = winfunc(dt[ind],l)
+            data_smooth[i] = (data[ind]*w).sum()/(w.sum())
+
+    else:
+        I = data.shape[1]
+        for i in range(I):
+            data_smooth[:,i] = window_1Dmean(data[:,i],
+                    l = l,
+                    t = t,
+                    method = method,
+                    axis = 0,
+                    parallel=parallel)
+
+    return data_smooth
