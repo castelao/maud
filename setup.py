d9175db87f7ea21dc8e7bb903c96b9eb1c92ce71
Capt. Dr. Guilherme Castelao <guilherme@castelao.net>
2013-09-06 19:48:23 -0300

    Can't install with old Distribute.

--- setup.py
+++ setup.py
@@ -21,11 +21,12 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
-version='0.6.5'
+version='0.6.6'
 
 requires = [
     'numpy>=1.1',
     'fluid>=0.2',
+    'distribute>=0.6.40',
     ]
 
 setup(
