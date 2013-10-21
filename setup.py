302c63ed04a142b28c05c6ee2571d86228812951
Capt. Dr. Guilherme Castelao <guilherme@castelao.net>
2013-10-21 00:02:04 -0200

    Minor update to reflect the multiprocessing on window_mean_2D_latlon

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
-version='0.6.8'
+version='0.6.9'
 
 requires = [
     'numpy>=1.1',
