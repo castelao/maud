37975bc32957d806efa742230b573f8793140fa8
Capt. Dr. Guilherme Castelao <guilherme@castelao.net>
2014-07-01 22:42:47 -0300

    Minor upgrade.

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
-version='0.8.8'
+version='0.8.9'
 
 requires = [
     'numpy>=1.1',
