ef2ee35e371a048be8e12587d9aa44ae4094a325
Bia <melovillasboas@gmail.com>
2014-08-24 17:45:19 -0300

    not ready yet. All commented

--- tests/test_triangular.py
+++ tests/test_triangular.py
@@ -7,12 +7,12 @@ from maud.window_func import _weight_triangular
 
 # Gotta adjust the coeficcients to the triangular window !!!
 
-def test_knownanswer():
-    r = array([-3, -2, -1, 0, 1, 2, 3, 100])
-    l = 5
-    w = _weight_triangular(r,l)
-    d = w - array([ 0., 0.0954915, 0.6545085, 1., 0.6545085, 0.0954915, 0., 0.])
-    assert absolute(d).max() < 1e-8
+#def test_knownanswer():
+#    r = array([-3, -2, -1, 0, 1, 2, 3, 100])
+#    l = 5
+#    w = _weight_triangular(r,l)
+#    d = w - array([ 0., 0.0954915, 0.6545085, 1., 0.6545085, 0.0954915, 0., 0.])
+#    assert absolute(d).max() < 1e-8
 
 
 #def test_PxC(N=50):
@@ -34,14 +34,14 @@ def test_knownanswer():
 #        d = w - w2
 #        assert absolute(d) < 1e-8
 
-def out_of_window():
-    r = 5*(2*random(10)-1)
-    l = 10*random()
-    w = _weight_triangular(r,l)
+#def out_of_window():
+#    r = 5*(2*random(10)-1)
+#    l = 10*random()
+#    w = _weight_triangular(r,l)
 #    cw = c_weight_triangular(r,l)
-    ind = r>l/2
-    assert (w[ind]==0).all()
-    assert (cw[ind]==0).all()
+#    ind = r>l/2
+#    assert (w[ind]==0).all()
+#    assert (cw[ind]==0).all()
 
 # Question: _weight_triangular(ma.masked_all(3), 5) should return a masked array?
 # _weight_triangular(ma.masked_all(4),5)
