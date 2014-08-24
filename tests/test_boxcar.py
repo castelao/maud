90c2485b154794a56d97e3d1ab6a182cc73c7b0d
Bia <melovillasboas@gmail.com>
2014-08-24 17:31:37 -0300

    fixing test_boxcar

--- tests/test_boxcar.py
+++ tests/test_boxcar.py
@@ -11,7 +11,7 @@ def test_knownanswer():
     r = array([-3, -2, -1, 0, 1, 2, 3, 100])
     l = 5
     w = _weight_boxcar(r,l)
-    d = w - array([ 0., 0.0954915, 0.6545085, 1., 0.6545085, 0.0954915, 0., 0.])
+    d = w - array([ 0., 1., 1., 1., 1., 1., 0., 0.])
     assert absolute(d).max() < 1e-8
 
 
