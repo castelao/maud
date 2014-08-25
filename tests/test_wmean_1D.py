74b5d64221ec253a21f3f79262138e56ac9be23a
Gui <guilherme@castelao.net>
2014-08-25 09:42:40 -0300

    More tests @ test_wmean_1D.

--- tests/test_wmean_1D.py
+++ tests/test_wmean_1D.py
@@ -4,6 +4,7 @@
 import numpy as np
 from numpy import ma
 from numpy import pi
+from numpy.random import random
 import maud
 import cmaud
 
@@ -29,6 +30,7 @@ def test_answer():
     assert abs(err).mean() < 1e-10
     assert abs(err).var() < 1e-10
     assert abs(err).max() < 1e-10
+
 
 def test_pass_all():
     """
@@ -60,3 +62,53 @@ def test_sin_diff():
     h = maud.wmean_1D(y, l=l, axis=0, method='hamming')
     err = S1 - h
     assert (abs(err).mean() < Z).all
+
+def test_mask_at_interp():
+    """ Test the behavior of masked points with interp on|off
+
+        As long as the filter is wide enough to capture at least
+          one data point per point, the interp=True will return
+    """
+    N = 25
+    t = np.arange(N)
+    y = random(N)
+    yn = y[y.argsort()[-5]]
+    y = ma.masked_greater(y, yn)
+    h = maud.wmean_1D(y, t=t, l=11, interp=False)
+    assert (y.mask == h.mask).all()
+    h = maud.wmean_1D(y, t=t, l=5, interp=True)
+    assert (~h.mask).all()
+
+
+def test_ones(N=25):
+    """ The energy must be preserved
+
+        Therefore, an array of ones must return only ones, even if
+          the input has mask, and with interp.
+    """
+
+    l = N/2
+
+    # Ones array
+    t = np.arange(N)
+    y = np.ones(N)
+    h = maud.wmean_1D(y, t=t, l=l)
+    assert (h == 1).all()
+
+    # Ones masked array with random masked positions
+    tmp = random(N)
+    # Select the top 1 third of the positions
+    thr = tmp[tmp.argsort()[-round(N/3.)]]
+
+    y = ma.masked_array(y, tmp>=thr)
+    h = maud.wmean_1D(y, t=t, l=l)
+    assert (h == 1).all()
+
+    # With interp, the energy should also be preserved
+    h = maud.wmean_1D(y, t=t, l=l, interp=True)
+    assert (h == 1).all()
+
+    # Masked values should not interfere in the filtered output.
+    y.data[y.mask==True] = 1e10
+    h = maud.wmean_1D(y, t=t, l=l)
+    assert (h == 1).all()
