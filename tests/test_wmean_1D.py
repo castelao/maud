c24a1f19f1fb6c4105aa6d5bf0345c1da5c49206
Gui <guilherme@castelao.net>
2014-08-26 09:24:23 -0300

    Reordering tests @ test_wmean_1D.

--- tests/test_wmean_1D.py
+++ tests/test_wmean_1D.py
@@ -8,6 +8,107 @@ from numpy.random import random
 import maud
 import cmaud
 
+def test_mask(N=10):
+    l = 5
+    t = np.arange(N)
+    y = np.ones(N)
+    h = maud.wmean_1D(y, t=t, l=l)
+    assert type(h) is np.ndarray
+
+    y = ma.array(y)
+    h = maud.wmean_1D(y, t=t, l=l)
+    assert type(h) == ma.MaskedArray
+    assert ~h.mask.any()
+
+    y.mask = ma.getmaskarray(y)
+    y.mask[0] = True
+    h = maud.wmean_1D(y, t=t, l=l)
+    assert h[0].mask == True
+    assert ~h[1:].mask.any()
+
+
+def eval_ones(y, t, l):
+
+    h = maud.wmean_1D(y, t=t, l=l)
+    assert (h == 1).all()
+
+    # Ones masked array with random masked positions
+    tmp = random(y.shape)
+    # Select the top 1 third of the positions
+    thr = np.percentile(tmp, 70)
+
+    y = ma.masked_array(y, tmp>=thr)
+    h = maud.wmean_1D(y, t=t, l=l)
+    assert (h == 1).all()
+
+    # Masked values should not interfere in the filtered output.
+    y.data[y.mask==True] = 1e10
+    h = maud.wmean_1D(y, t=t, l=l)
+    assert (h == 1).all()
+
+    # With interp, the energy should also be preserved
+    h = maud.wmean_1D(y, t=t, l=l, interp=True)
+    assert (h == 1).all()
+
+def test_ones(N=25):
+    """ The energy must be preserved
+
+        Therefore, an array of ones must return only ones, even if
+          the input has mask, and with interp.
+    """
+    l = N/2
+
+    print("Testing 1D array")
+    t = np.arange(N)
+    y = np.ones(N)
+    eval_ones(y, t, l)
+
+    #print("Testing 2D array")
+    t = np.arange(N)
+    y = np.ones((N,3))
+    eval_ones(y, t, l)
+
+
+def test_mask_at_interp():
+    """ Test the behavior of masked points with interp on|off
+
+        As long as the filter is wide enough to capture at least
+          one data point per point, the interp=True will return
+    """
+    N = 25
+    t = np.arange(N)
+    y = random(N)
+    yn = y[y.argsort()[-5]]
+    y = ma.masked_greater(y, yn)
+    # Equivalent to interp=False
+    h = maud.wmean_1D(y, t=t, l=11)
+    assert (y.mask == h.mask).all()
+    h = maud.wmean_1D(y, t=t, l=5, interp=True)
+    assert (~h.mask).all()
+
+
+def test_Serial_x_Parallel(N=10):
+    """
+
+        Improve this. Should include more possibilities like:
+          different arrays shapes, l, input types(array x MA)
+    """
+    l = 5
+    t = np.arange(N)
+    y = random(N)
+    h_serial = maud.wmean_1D_serial(y, t=t, l=l)
+    h = maud.wmean_1D(y, t=t, l=l)
+    assert (h_serial == h).all()
+
+def Python_x_Cython(N=10):
+    l = 5
+    # ATENTION, in the future I should not force t to be np.float.
+    t = np.arange(N, dtype=np.float)
+    y = random((N, 3))
+    h = maud.wmean_1D_serial(y, t=t, l=l)
+    ch = cmaud.wmean_1D(y, t=t, l=l)
+    assert (h == ch).all()
+
 
 def test_answer():
     """
@@ -63,74 +164,3 @@ def test_sin_diff():
     err = S1 - h
     assert (abs(err).mean() < Z).all
 
-def test_mask_at_interp():
-    """ Test the behavior of masked points with interp on|off
-
-        As long as the filter is wide enough to capture at least
-          one data point per point, the interp=True will return
-    """
-    N = 25
-    t = np.arange(N)
-    y = random(N)
-    yn = y[y.argsort()[-5]]
-    y = ma.masked_greater(y, yn)
-    h = maud.wmean_1D(y, t=t, l=11, interp=False)
-    assert (y.mask == h.mask).all()
-    h = maud.wmean_1D(y, t=t, l=5, interp=True)
-    assert (~h.mask).all()
-
-
-def test_ones(N=25):
-    """ The energy must be preserved
-
-        Therefore, an array of ones must return only ones, even if
-          the input has mask, and with interp.
-    """
-
-    l = N/2
-
-    # Ones array
-    t = np.arange(N)
-    y = np.ones(N)
-    h = maud.wmean_1D(y, t=t, l=l)
-    assert (h == 1).all()
-
-    # Ones masked array with random masked positions
-    tmp = random(y.shape)
-    # Select the top 1 third of the positions
-    thr = tmp[tmp.argsort()[-round(N/3.)]]
-
-    y = ma.masked_array(y, tmp>=thr)
-    h = maud.wmean_1D(y, t=t, l=l)
-    assert (h == 1).all()
-
-    # With interp, the energy should also be preserved
-    #h = maud.wmean_1D(y, t=t, l=l, interp=True)
-    #assert (h == 1).all()
-
-    # Masked values should not interfere in the filtered output.
-    y.data[y.mask==True] = 1e10
-    h = maud.wmean_1D(y, t=t, l=l)
-    assert (h == 1).all()
-
-
-def Serial_x_Parallel(N=10):
-    """
-
-        Improve this. Should include more possibilities like:
-          different arrays shapes, l, input types(array x MA)
-    """
-    t = np.arange(N)
-    y = random(N)
-    h_serial = maud.wmean_1D_serial(y, t=t, l=l)
-    h = maud.wmean_1D(y, t=t, l=l)
-    assert (h_serial == h).all()
-
-def test_Python_x_Cython(N=10):
-    l = 5
-    # ATENTION, in the future I should not force t to be np.float.
-    t = np.arange(N, dtype=np.float)
-    y = random((N, 3))
-    h = maud.wmean_1D_serial(y, t=t, l=l)
-    ch = cmaud.wmean_1D(y, t=t, l=l)
-    assert (h == ch).all()
