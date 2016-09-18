74934e89cace4a51f0a2bfeed8ca86a3571ae9f3
ZeDias <jdiasn@gmail.com>
2016-09-18 16:28:24 -0300

    Test improvement to work with/without cython.

--- tests/test_haversine.py
+++ tests/test_haversine.py
@@ -4,8 +4,17 @@
 import numpy as np
 from numpy.random import random
 from maud.distance import haversine
-from maud.cdistance import haversine as c_haversine
-from maud.cdistance import haversine_scalar
+
+
+try:
+    import cython
+    with_cython = True
+except:
+    with_cython = False
+
+if with_cython:
+    from maud.cdistance import haversine as c_haversine
+    from maud.cdistance import haversine_scalar
 
 def test_zerodistance(N=25):
     """ Distance from one point to itself
@@ -14,7 +23,10 @@ def test_zerodistance(N=25):
         lon = 540*random() - 180
         lat = 180*random() - 90
         assert (haversine(lon, lat, lon, lat) == 0.0)
-        assert (haversine_scalar(lon, lat, lon, lat) == 0.0)
+
+	if with_cython:
+             #assert (c_haversine(lon, lat, lon, lat) == 0.0)
+	     assert (haversine_scalar(lon, lat, lon, lat) == 0.0)
 
 
 def test_360offset(N=25):
@@ -26,14 +38,19 @@ def test_360offset(N=25):
     Y = np.append([-90, 0, 90], Y)
     for x, y in zip(X, Y):
         d = haversine(x, y, x+360, y)
-        c_d = haversine_scalar(x, y, x+360, y)
         assert (d<1e-3) # Less then a milimiter
-        assert (c_d<1e-3) # Less then a milimiter
+
+        if with_cython:
+            c_d = haversine_scalar(x, y, x+360, y)
+            assert (c_d<1e-3) # Less then a milimiter
 
 
 def test_PxC(N=25):
     """ Python and Cython should give the same answer
     """
+    if not with_cython:
+        return
+
     lon0 = 10
     lat0 = -25
     Lon = 540*random(N) - 180
@@ -77,8 +94,9 @@ def  test_onedegreedistance(N=25):
     Lat = 89.5*(2*Lat-1)
     for lon, lat in zip(Lon, Lat):
         d = haversine(lat-0.5, lon, lat+0.5, lon)
-        c_d = haversine_scalar(lat-0.5, lon, lat+0.5, lon)
-
         assert abs(d-111195)<0.1
-        assert abs(c_d-111195)<0.1
+	
+        if with_cython:
+            c_d = haversine_scalar(lat-0.5, lon, lat+0.5, lon)
+            assert abs(c_d-111195)<0.1
 
